9cab86081613b77ea23d83e84f4615a6c4b347ae
Joshua Searles <jrsearles@comcast.net>
2015-09-07 22:56:29 -0400

    hoist strict directive

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ var fs = require("vinyl-fs");
 var through = require("through2");
 var merge = require("merge-stream");
 var path = require("path");
+var acorn = require("acorn");
 
 var root = "node_modules/test262/test/suite/";
+
+function getPrefix (src) {
+	try {
+		var ast = acorn.parse(src);
+		var body = ast.body[0];
+		if (body
+			&& body.type === "ExpressionStatement"
+			&& body.expression.type === "Literal"
+			&& body.expression.value === "use strict") {
+
+			// hoist strict directive so it precedes test harness
+			return body.expression.raw + "\n";
+		}
+	}	catch (err) { }
+
+	return "";
+}
 
 module.exports = function test262Streamer (opt) {
 	opt = opt || {};
@@ -22,9 +40,12 @@ module.exports = function test262Streamer (opt) {
 
 	var stream2 = fs.src(files)
 			.pipe(through.obj(function (file, enc, done) {
-				var src = harness + file.contents.toString();
+				var src = file.contents.toString();
 
-				file.contents = new Buffer(src);
+				// need to hoist strict directive
+				var pre = getPrefix(src);
+
+				file.contents = new Buffer(pre + harness + src);
 				done(null, file);
 			}));
 
